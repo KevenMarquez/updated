d244e697da0faadde3999838e17f8910d3a88ba1
KevenMarquez <KevenMarquez902@gmail.com>
2023-09-02 10:50:56 -0300

    updated code

---
+++ Projeto/Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace Projeto
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            sideBar = new FlowLayoutPanel();
+            panel1 = new Panel();
+            button1 = new Button();
+            panel2 = new Panel();
+            panel4 = new Panel();
+            button3 = new Button();
+            panel5 = new Panel();
+            button4 = new Button();
+            button2 = new Button();
+            pictureBox1 = new PictureBox();
+            label1 = new Label();
+            SidebarTimer = new System.Windows.Forms.Timer(components);
+            sideBar.SuspendLayout();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            panel4.SuspendLayout();
+            panel5.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // sideBar
+            // 
+            sideBar.BackColor = SystemColors.AppWorkspace;
+            sideBar.Controls.Add(panel2);
+            sideBar.Controls.Add(panel1);
+            sideBar.Controls.Add(panel4);
+            sideBar.Controls.Add(panel5);
+            sideBar.Controls.Add(button2);
+            sideBar.Dock = DockStyle.Left;
+            sideBar.ForeColor = SystemColors.ControlText;
+            sideBar.Location = new Point(0, 0);
+            sideBar.Name = "sideBar";
+            sideBar.Size = new Size(200, 450);
+            sideBar.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(button1);
+            panel1.Location = new Point(3, 109);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(197, 32);
+            panel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            button1.Image = Properties.Resources.icons8_casa_25;
+            button1.ImageAlign = ContentAlignment.MiddleLeft;
+            button1.Location = new Point(-3, -7);
+            button1.Name = "button1";
+            button1.Padding = new Padding(30, 0, 0, 0);
+            button1.Size = new Size(200, 45);
+            button1.TabIndex = 1;
+            button1.Text = "        Home";
+            button1.TextAlign = ContentAlignment.MiddleLeft;
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(label1);
+            panel2.Controls.Add(pictureBox1);
+            panel2.Location = new Point(3, 3);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(200, 100);
+            panel2.TabIndex = 2;
+            // 
+            // panel4
+            // 
+            panel4.Controls.Add(button3);
+            panel4.Location = new Point(0, 151);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(197, 32);
+            panel4.TabIndex = 0;
+            // 
+            // button3
+            // 
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            button3.Image = (Image)resources.GetObject("button3.Image");
+            button3.ImageAlign = ContentAlignment.MiddleLeft;
+            button3.Location = new Point(1, -4);
+            button3.Name = "button3";
+            button3.Padding = new Padding(30, 0, 0, 0);
+            button3.Size = new Size(199, 36);
+            button3.TabIndex = 1;
+            button3.Text = "        Settings";
+            button3.TextAlign = ContentAlignment.MiddleLeft;
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // panel5
+            // 
+            panel5.Controls.Add(button4);
+            panel5.Location = new Point(1, 187);
+            panel5.Name = "panel5";
+            panel5.Size = new Size(197, 32);
+            panel5.TabIndex = 0;
+            // 
+            // button4
+            // 
+            button4.FlatStyle = FlatStyle.Flat;
+            button4.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            button4.Image = (Image)resources.GetObject("button4.Image");
+            button4.ImageAlign = ContentAlignment.MiddleLeft;
+            button4.Location = new Point(-1, -12);
+            button4.Name = "button4";
+            button4.Padding = new Padding(30, 0, 0, 0);
+            button4.Size = new Size(200, 44);
+            button4.TabIndex = 1;
+            button4.Text = "        Help";
+            button4.TextAlign = ContentAlignment.MiddleLeft;
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            button2.Image = (Image)resources.GetObject("button2.Image");
+            button2.ImageAlign = ContentAlignment.MiddleLeft;
+            button2.Location = new Point(1, 217);
+            button2.Name = "button2";
+            button2.Padding = new Padding(30, 0, 0, 0);
+            button2.Size = new Size(199, 38);
+            button2.TabIndex = 1;
+            button2.Text = "        About";
+            button2.TextAlign = ContentAlignment.MiddleLeft;
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Cursor = Cursors.Hand;
+            pictureBox1.Image = Properties.Resources.icons8_cardápio_25__1_;
+            pictureBox1.Location = new Point(35, 55);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(25, 25);
+            pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(66, 59);
+            label1.Name = "label1";
+            label1.Size = new Size(50, 21);
+            label1.TabIndex = 1;
+            label1.Text = "Menu";
+            label1.Click += label1_Click;
+            // 
+            // SidebarTimer
+            // 
+            SidebarTimer.Tick += SidebarTimer_Tick;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(sideBar);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            sideBar.ResumeLayout(false);
+            panel1.ResumeLayout(false);
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            panel4.ResumeLayout(false);
+            panel5.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private FlowLayoutPanel sideBar;
+        private Panel panel2;
+        private Panel panel1;
+        private Button button1;
+        private Panel panel4;
+        private Button button3;
+        private Panel panel5;
+        private Button button4;
+        private PictureBox pictureBox1;
+        private Button button2;
+        private Label label1;
+        private System.Windows.Forms.Timer SidebarTimer;
+    }
+}
---
+++ Projeto/Form1.cs
@@ -0,0 +1,39 @@
+namespace Projeto
+{
+    public partial class Form1 : Form
+    {
+        bool SidebarExpand;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SidebarTimer_Tick(object sender, EventArgs e)
+        {
+            if (SidebarExpand)
+            {
+                
+            }
+
+        }
+    }
+}
